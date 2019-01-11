cc9129814847cace29f1406ff7c260d8834a9413
ieso1994 <ieso1994@live.com>
2019-01-11 09:12:44 +0800

    1

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ieso
+ * Date: 2019/1/11
+ * Time: 9:11
+ */
+
+namespace App\Http\Controllers;
+
+
+class TestController
+{
+    public function test(){
+
+    }
+}
